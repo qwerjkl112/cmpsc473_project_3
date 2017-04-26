359e2407b87bcb1d333c89e82279115a99b85232
qwerjkl112 <qwerjkl112@gmail.com>
2017-04-26 14:44:06 -0400

    Update myVirtualMemoryManagerInterface.cpp

--- myVirtualMemoryManagerInterface.cpp
+++ myVirtualMemoryManagerInterface.cpp
@@ -3,7 +3,7 @@
 //  File           : myVirtualMemoryManager.cpp
 //  Description    : Defines all the functions in "myVirtualMemoryManager.h"
 //
-//  Author         : Jeff Li, Frank Guo, Chen Dikan
+//  Author         : Jeff Li, Chao Guo, Chen Dikan
 //  Last Modified  : 04/28/2017
 
 #include "myVirtualMemoryManagerInterface.h"
